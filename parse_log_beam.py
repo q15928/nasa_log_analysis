565f4674b9ea2964d4a0273bfb60af65f2b0c32f
Jason Feng <q15928@hotmail.com>
2019-04-26 22:11:34 +1000

    Update the pipeline to read from pub/sub

--- parse_log_beam.py
+++ parse_log_beam.py
@@ -1,9 +1,8 @@
-from __future__ import print_function
-from __future__ import absolute_import
-
 import argparse
 import logging
 import re
+from datetime import datetime
+import pytz
 
 from past.builtins import unicode
 
@@ -17,30 +16,38 @@ from apache_beam.options.pipeline_options import SetupOptions
 from apache_beam import coders
 
 
-def parse_timestamp():
+def parse_timestamp(str_ts):
     """
-    Convert Common Log time format into a Python datetime object
+    Convert Common Log time format into a Python datetime object in UTC
     """
-    pass
+    local_ts = datetime.strptime(str_ts, '%d/%b/%Y:%H:%M:%S %z')
+    return local_ts.astimezone(pytz.utc).isoformat()
+
+
+def str_to_int(str_num):
+    try:
+        return int(str_num)
+    except Exception as e:
+        return 0
+
 
 class ParseAndFilterDoFn(beam.DoFn):
     def process(self, element):
         pattern = rb'(^\S+?)\s.+?\[(.+)\]\s+"(.+?)\s(\S+)\s*(.*)"\s(\d+)\s(.+)'
         match = re.match(pattern, element)
         if match is not None:
-            yield list(match.groups())
+            res = {}
+            res['host'] = match.group(1).decode('utf-8')
+            res['utc_timestamp'] = parse_timestamp(match.group(2).decode('utf-8'))
+            res['action'] = match.group(3).decode('utf-8')
+            res['uri'] = match.group(4).decode('utf-8')
+            res['protocol'] = match.group(5).decode('utf-8')
+            res['status'] = match.group(6).decode('utf-8')
+            res['size'] = str_to_int(match.group(7).decode('utf-8'))
+            yield res
         else:
             return
 
-
-def parse_one_record(one_rec):
-    """
-    Parse one row of log into fields according to Common Logfile Format
-    """
-    pattern = rb'(^\S+?)\s.+?\[(.+)\]\s+"(.+?)\s(\S+)\s*(.*)"\s(\d+)\s(.+)'
-    line = one_rec
-    match = re.match(pattern, line)
-    return list(match.groups())
 
 def run(argv=None):
     """
@@ -53,6 +60,10 @@ def run(argv=None):
     parser.add_argument('--output',
                         dest='output',
                         help='Output file to write results to')
+    parser.add_argument('--input_subscription',
+                        dest='input_subscription',
+                        help=('Input PubSub subscription of the form '
+                              '"projects/<PROJECT>/subscriptions/<SUBSCRIPTION>."'))
     known_args, pipeline_args = parser.parse_known_args(argv)
     pipeline_args.extend([
         '--runner=DirectRunner',
@@ -64,16 +75,34 @@ def run(argv=None):
     pipeline_options = PipelineOptions(pipeline_args)
     pipeline_options.view_as(SetupOptions).save_main_session = True
 
+    # Specification for table in BigQuery
+    table_spec = 'jf-project-20190218:nasa_logs.raw_access_logs'
+    table_schema = 'host:STRING, utc_timestamp:TIMESTAMP, action:STRING, uri:STRING, protocol:STRING, status:STRING, size:INTEGER'
+    
     with beam.Pipeline(options=pipeline_options) as p:
         # Read the text file[pattern] into a PCollection.
-        lines = p | ReadFromText(known_args.input, coder=coders.BytesCoder())
+        if known_args.input_subscription:
+            lines = (
+                p 
+                | ReadFromPubSub(
+                    subscription=known_args.input_subscription)
+                .with_output_types(bytes))
+        else:
+            lines = (
+                p 
+                | ReadFromText(known_args.input, coder=coders.BytesCoder()))
 
         output = (
             lines 
             | 'parse_filter' >> beam.ParDo(ParseAndFilterDoFn()))
             # | 'parse' >> (beam.Map(parse_one_record)))
 
-        output | WriteToText(known_args.output)
+        # output | WriteToText(known_args.output)
+        output | WriteToBigQuery(
+            table_spec,
+            schema=table_schema,
+            write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
+            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
 
 
 if __name__ == '__main__':
